b0f36400cfec92224b49d3d18958b93c39cd5954
levantai1010 <levantai101001@gmail.com>
2021-09-19 10:32:12 +0700

    add auth

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const authenticate = (req, res, next) => {
+  //   const token = req.header("token");
+  const token = req.header("token");
+  try {
+    const secrectKey = "secrect";
+    const decode = jwt.verify(token, secrectKey);
+    console.log("Decode: ", decode);
+    if (decode) {
+      req.user = decode;
+      // res.status(200).send(decode);
+      next();
+    } else {
+      res.status(401).send("Login failed");
+    }
+  } catch (error) {
+    res.status(401).send("Login failed");
+  }
+};
+const authorize = (arrRole) => (req, res, next) => {
+  const { user } = req;
+  if (arrRole.findIndex((ele) => ele === user.role) > -1) {
+    next();
+  } else {
+    res.status(403).send("Not permission");
+  }
+};
+module.exports = { authenticate, authorize };
--- routers/user.router.js
+++ routers/user.router.js
@@ -5,12 +5,25 @@ const {
   removeUser,
   updateUser,
 } = require("../controllers/user.controller");
+const { authenticate, authorize } = require("../middleware/auth.middleware");
 const { checkExist } = require("../middleware/check-exist.middleware");
 const userRouter = express.Router();
 userRouter.get("/", getUserList);
 userRouter.post("/create-user", createUser);
-userRouter.delete("/remove-user/:id", checkExist, removeUser);
-userRouter.put("/update-user/:id", checkExist, updateUser);
+userRouter.delete(
+  "/remove-user/:id",
+  checkExist,
+  authenticate,
+  authorize(["ADMIN", "SUPPERADMIN"]),
+  removeUser
+);
+userRouter.put(
+  "/update-user/:id",
+  checkExist,
+  authenticate,
+  authorize(["ADMIN", "SUPPERADMIN"]),
+  updateUser
+);
 module.exports = {
   userRouter,
 };
